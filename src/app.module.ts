26c444e91ae5f4af0ec1617ae5f9932644e95895
Fortino Inocente Vicente <fortino.inocente@gmail.com>
2022-07-21 21:38:12 -0600

    MongooseModule.forRootAsync

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,11 @@ import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost:27017/products')],
+  imports: [MongooseModule.forRootAsync({
+    useFactory: () => ({
+      uri: 'mongodb://localhost:27017/products',
+    }),
+  }), ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
